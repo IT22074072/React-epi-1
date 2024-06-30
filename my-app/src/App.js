f603a18abe9a3227169f590b5d71e7bd9ec91489
IT22074072 <it22074072@my.sliit.lk>
2024-06-30 08:47:37 +0530

    props(arguments in a component)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,21 @@ function App() {
   //this is how we call the User Component
   return (
     <div className="App">
-      <User />
-      <User />
-      <User />
+      <User name="Dinithi" age = {21} email = "dini@gmail.com"/>
+      <User name="Piyumi" age = {25} email = "pipi@gmail.com"/>
+      <User name="JK" age = {26} email = "jk@gmail.com"/>
     </div>
   );
 }
 
 //this returns ui stuff(some html code)
-const User = () => {
+const User = (props) => {
   return (
-    <div>
-      <h1>Dinithi</h1>
-      <h2>21</h2>
-      <h2>dini@gmail.com</h2>
+    <div> 
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      <h2>{props.email}</h2>
+      <br/>
     </div>
   );
 };
