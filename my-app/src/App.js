459b4db654a39f322e743b8494bbc62f0f138ba3
IT22074072 <it22074072@my.sliit.lk>
2024-06-30 08:01:50 +0530

    JSX, Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,25 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
+  //this is how we call the User Component
   return (
     <div className="App">
-      Hello world
+      <User />
+      <User />
+      <User />
     </div>
   );
 }
+
+//this returns ui stuff(some html code)
+const User = () => {
+  return (
+    <div>
+      <h1>Dinithi</h1>
+      <h2>21</h2>
+      <h2>dini@gmail.com</h2>
+    </div>
+  );
+};
 
 export default App;
